9e78b9f9943ff5fa92694bede31dbf472e520eb3
rhit-franklkj <franklkj@rose-hulman.edu>
2025-02-13 14:40:35 -0500

    clean up print statements and also bootstrap angry again.

--- budget-goal/src/components/input.jsx
+++ budget-goal/src/components/input.jsx
@@ -19,25 +19,19 @@ export default function InputMoney(props){
         <div className="col-auto">
         <label className="fs-2"></label>
         <div className="d-flex gap-2">
-          <input type="text" id="inputDesc" className="form-control fs-5" placeholder="Add description"/>
-          <input type="text" id="inputMoney" className="form-control fs-5" placeholder="Enter Amount"/>
+          <input type="text" id="inputDesc" className="form-control fs-5" placeholder="Add description" required/>
+          <input type="text" id="inputMoney" className="form-control fs-5" placeholder="Enter Amount" required/>
           <button className="btn fs-5" type="button" style={{ backgroundColor: "#2C3930", color: "white" } } onClick={()=> {
 
                 if( document.getElementById("dropdown").textContent === "Income"){
-                    console.log("add moneys");
                     const desc = document.getElementById("inputDesc").value;
                     const money = document.getElementById("inputMoney").value; 
-                    console.log(desc + " " + money );
                     moneyManager.add("Income", desc, money); 
                 } else if ( document.getElementById("dropdown").textContent === "Expense") {
-                    console.log("spend moneys");
                     const desc = document.getElementById("inputDesc").value;
                     const money = document.getElementById("inputMoney").value; 
-                    console.log(desc + " " + money );
                     moneyManager.add("Expense", desc, money); 
-                } else {
-                    console.log("why god why");
-                }
+                } 
           }}>Submit</button>
         </div>
         </div>
@@ -47,6 +41,5 @@ export default function InputMoney(props){
 }
 
 InputMoney.propTypes = {
-
   manager: PropTypes.object.isRequired
 }
