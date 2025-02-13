99b4ec75d238d91653dda69603574af37765537b
rhit-kaurj1 <kaurj1@rose-hulman.edu>
2025-02-12 20:01:23 -0500

    downloaded a weird package idk

---
+++ budget-goal/src/goal.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './bootstrap.min.css'
+import './bootstrap.bundle.min.js'
+import './App.css'
+
+function goal() {
+  return (
+    <>
+    <h1>Goal Page</h1>
+    </>
+  )
+}
